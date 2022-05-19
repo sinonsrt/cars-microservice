2fa6b25e83ed71b9420e54c51413739aea3fa859
Lucas Marchiori <sinonsrt@hotmail.com>
2022-05-19 16:25:58 -0300

    start buyer-microservice

--- seller-microservice/src/seller/seller.controller.ts
+++ seller-microservice/src/seller/seller.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { SellerService } from './seller.service';
 import { CreateSellerDto } from './dto/create-seller.dto';
 import { UpdateSellerDto } from './dto/update-seller.dto';
